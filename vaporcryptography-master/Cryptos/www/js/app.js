280a2ed2f42f98758bfe739c2e83bb9714877d30
njk345 <njk@Nicks-MacBook-Retina.local>
2016-05-24 21:37:28 -0400

    To be reverted

--- vaporcryptography-master/Cryptos/www/js/app.js
+++ vaporcryptography-master/Cryptos/www/js/app.js
@@ -21,3 +21,55 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
     }
   });
 })
+
+app.directive('caesarCipherQuiz', function(caesarQuizFactory) {
+  return {
+      restrict: 'AE',
+      scope: {},
+      templateUrl: 'template.html',
+      link: function(scope, elem, attrs) {
+        scope.start = function() {
+          scope.id = 0;
+          scope.inProgress = true;
+          scope.getQuestion();
+        };
+   
+        scope.reset = function() {
+          scope.inProgress = false;
+          scope.score = 0;
+        }
+   
+        scope.getQuestion = function() {
+          var q = quizFactory.getQuestion(scope.id);
+          if(q) {
+            scope.question = q.question;
+            scope.answer = q.answer;
+          } else {
+            scope.inProgress = false;
+          }
+        };
+   
+        scope.checkAnswer = function() {
+          if(!$('input[name=answer]:checked').length) return;
+   
+          var ans = $('input[name=answer]:checked').val();
+   
+          if(ans == scope.options[scope.answer]) {
+            scope.score++;
+            scope.correctAns = true;
+          } else {
+            scope.correctAns = false;
+          }
+   
+          scope.answerMode = false;
+        };
+   
+        scope.nextQuestion = function() {
+          scope.id++;
+          scope.getQuestion();
+        }
+   
+        scope.reset();
+      }
+    }
+})
